24e634ff32bee10ed7c317c5a1c81ec462e69a9a
adorium <adorium@gmail.com>
2022-11-08 23:26:00 +0900

    vue-js login

---
+++ vue-store/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <LogIn v-on:loginInfo="requestLogin" msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import LogIn from './components/LogIn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LogIn
+  },
+  methods: {
+    requestLogin:function(user) {
+      console.log(user);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-store/src/components/LogIn.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="page-container login">
+		<div class="login-box">
+			<div class="logo">
+				<img src="../assets/images/logo.png"/>
+			</div>
+			<form class="login-form" action="" v-on:submit.prevent="submitForm">
+                <div class="form-group">
+                    <input type="email" id="userid" v-model="userid" autocomplete="off" placeholder="이메일 ID" class="form-control form-control-solid placeholder-no-fix">
+                </div>
+                <div class="form-group">
+                    <input type="password" autocomplete="off" placeholder="비밀번호" name="password" class="form-control form-control-solid placeholder-no-fix">
+                </div>
+                <div class="form-actions">
+                    <button type="submit" class="btn navy btn-block uppercase">로그인</button>
+                </div>
+                <div class="form-actions">
+                    <div class="pull-left">
+                        <input type="checkbox" name="remember" value="1"><span style="padding: 3px 0px 5px;">아이디 저장</span>
+                    </div>
+                    <div class="pull-right">
+                        <a href="#/newPassword" id="forget-password" class="forget-password">
+                            <i class="fa fa-angle-right"></i> 비밀번호 재설정
+                        </a>
+                    </div>
+                </div>
+			</form>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+    name:'LogIn',
+    data:function() {
+        return {
+            userid:'',
+            userpw:'',
+        }
+    },
+    methods:{
+        submitForm: function() {
+            var user = { "userid":this.userid, "password":this.password, "name":"Instapay User" }
+            this.$emit('loginInfo', user);
+        }
+    }
+}
+</script>
